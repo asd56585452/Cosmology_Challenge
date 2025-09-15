f9516ae405c686cfa21e2c1a7c30db1513da7418
asd56585452 <52537259+asd56585452@users.noreply.github.com>
2025-09-15 21:22:20 +0800

    less epoch

--- Phase_1_Startingkit_WL_PSAnalysis.py
+++ Phase_1_Startingkit_WL_PSAnalysis.py
@@ -519,7 +519,7 @@ def main():
     # **Option 2: To load the entire training data and test data:** Set `USE_PUBLIC_DATASET = True` and specify a path where you will save the downloaded public data from Codabench. In the entire training data, there are $N_{\rm cosmo}=101$ cosmological models and $N_{\rm sys}=256$ realizations of nuisance parameters. The entire test data contains $N_{\rm test}=4000$ instances.
 
     # %%
-    USE_PUBLIC_DATASET = False
+    USE_PUBLIC_DATASET = True
 
     # USE_PUBLIC_DATASET = True
     PUBLIC_DATA_DIR = 'public_data/'  # This is only required when you set USE_PUBLIC_DATASET = True
@@ -651,7 +651,7 @@ def main():
     # %%
     # -- Hyperparameters --
     N_SPLITS = 5 # For 5-fold cross-validation
-    N_EPOCHS = 30 # A reasonable default. User may need to adjust for full training runs.
+    N_EPOCHS = 10 # A reasonable default. User may need to adjust for full training runs.
     BATCH_SIZE = 8 # Reduced batch size to prevent OOM error
     LEARNING_RATE = 1e-5
     RANDOM_SEED = 42
